cb93800b3cd159b325f9851c40ae1aaa1a345250
Erfan Nikaan <erfan2004nikan@gmail.com>
2025-03-15 23:18:28 +0330

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,16 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
-import { SliderComponent } from './slider/slider.component';
-import { ContentComponent } from './content/content.component';
-import { ExmpleComponent } from './exmple/exmple.component';
-import { FooterComponent } from './footer/footer.component';
-import { ProductComponent } from './exmple/product/product.component';
+import { MainComponent } from './main/main.component';
+
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, 
-    RouterLink,  
-    ProductComponent,
-    HeaderComponent,   
-    SliderComponent,   
-    ContentComponent,   
-    ExmpleComponent,   
-    FooterComponent],
+  imports: [HeaderComponent,
+    MainComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   schemas:[ CUSTOM_ELEMENTS_SCHEMA ]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,7 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { HeaderComponent } from './header/header.component';
-import { ProductComponent } from './exmple/product/product.component';
-import { AppComponent } from './app.component';
+
 
 
 export const routes: Routes = [
-    {
-        path:'',
-        component: AppComponent
-    },
-    {
-        path:'login',
-        component: LoginComponent
-    },
-    {
-        path:'header',
-        component: HeaderComponent
-    },
-    {
-        path:'product',
-        component: ProductComponent
-    }
+   
 ];
